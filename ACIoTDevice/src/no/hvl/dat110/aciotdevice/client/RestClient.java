afd591e095ca6f41850db9a8378c515f86c95e64
h131177 <131177@stud.hvl.no>
2020-05-09 00:47:01 +0200

    Step 4: Implementing device network communication

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,17 +1,42 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import okhttp3.*;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class RestClient {
 
 	public RestClient() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	private static String logpath = "/accessdevice/log";
 
 	public void doPostAccessEntry(String message) {
 
 		// TODO: implement a HTTP POST on the service to post the message
-		
+		OkHttpClient okClient = new OkHttpClient();
+		Gson gson = new Gson();
+		AccessMessage aMsg = new AccessMessage(message);
+		RequestBody reqBody = RequestBody.create(JSON, gson.toJson(aMsg));
+
+		Request request = new Request.Builder()
+				.url("http://localhost:8080/accessdevice/log/")
+				.post(reqBody)
+				.build();
+
+		System.out.println(request);
+
+		try (Response response = okClient.newCall(request).execute()) {
+			System.out.println(response.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static String codepath = "/accessdevice/code";
@@ -21,7 +46,23 @@ public class RestClient {
 		AccessCode code = null;
 		
 		// TODO: implement a HTTP GET on the service to get current access code
-		
+		OkHttpClient okClient = new OkHttpClient();
+		Gson gson = new Gson();
+
+		Request request = new Request.Builder()
+				.url("http://localhost:8080/accessdevice/code")
+				.get()
+				.build();
+
+		System.out.println(request);
+
+		try (Response response = okClient.newCall(request).execute()) {
+			String resBody = response.body().string();
+			System.out.println(resBody);
+			code = gson.fromJson(resBody, AccessCode.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return code;
 	}
 }
